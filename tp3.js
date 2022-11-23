f9549259cac0fd7034c10ec2fddeb1414b7100e2
4saisons91 <114591097+4saisons91@users.noreply.github.com>
2022-11-23 18:03:05 +0100

    Add files via upload

---
+++ tp3.js
@@ -0,0 +1,27 @@
+function permuter()
+{
+
+    var V3 = "";
+    a = document.getElementById("t1").value;
+    b = document.getElementById("t2").value;
+    V3=a
+    a=b
+    b=V3
+
+    t1.value = a;
+    t2.value=b
+
+}
+function parite()
+{
+
+        var v = Number(documentgetElementById("t1").value)
+        if (v % 2==0)
+
+            alert("Cenombre est pair");
+
+    else
+
+        alert("Ce nombre est impair");
+        
+}
